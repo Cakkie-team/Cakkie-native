d1dc93e7e47705866660251b98713630f722af11
vcky4 <okon.victor.u@gmail.com>
2023-10-31 00:34:15 +0100

    feet: add settings

---
+++ app/src/main/java/com/cakkie/datastore/SettingsConstants.kt
@@ -0,0 +1,7 @@
+package com.cakkie.datastore
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object SettingsConstants {
+    val TOKEN = stringPreferencesKey("TOKEN")
+}
--- app/src/main/java/com/cakkie/di/appModules.kt
+++ app/src/main/java/com/cakkie/di/appModules.kt
@@ -1,10 +1,16 @@
 package com.cakkie.di
 
+import com.cakkie.datastore.Settings
 import com.cakkie.ui.screens.splash.SplashViewModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+
+    single {
+        Settings(androidApplication())
+    }
 
     //inject SplashViewModel
     viewModel {
