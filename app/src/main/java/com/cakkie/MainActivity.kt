ebb48f57f9a467b5be9895edbf073d254191b282
vcky4 <okon.victor.u@gmail.com>
2023-10-31 00:06:04 +0100

    feet: create background

--- app/src/main/java/com/cakkie/MainActivity.kt
+++ app/src/main/java/com/cakkie/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.cakkie.ui.screens.NavGraphs
+import com.cakkie.ui.theme.BackgroundImageId
 import com.cakkie.ui.theme.CakkieTheme
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
@@ -25,10 +31,18 @@ class MainActivity : ComponentActivity() {
             CakkieTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    DestinationsNavHost(
-                        navGraph = NavGraphs.root,
-                        engine = rememberAnimatedNavHostEngine()
-                    )
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Image(
+                            painter = painterResource(id = BackgroundImageId(isSystemInDarkTheme())),
+                            contentDescription = "background",
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.FillBounds
+                        )
+                        DestinationsNavHost(
+                            navGraph = NavGraphs.root,
+                            engine = rememberAnimatedNavHostEngine()
+                        )
+                    }
                 }
             }
         }
