54d8c94c8401f9db86116932376bb7c2f5c17f40
vcky4 <okon.victor.u@gmail.com>
2023-10-30 21:44:17 +0100

    feet: splash screen

--- app/src/main/java/com/cakkie/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/cakkie/ui/splash/SplashScreen.kt
@@ -1,7 +1,13 @@
 package com.cakkie.ui.splash
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.cakkie.R
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 
@@ -9,5 +15,12 @@ import com.ramcosta.composedestinations.annotation.RootNavGraph
 @RootNavGraph(start = true)
 @Composable
 fun SplashScreen() {
-    Text(text = "Splash Screen")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.splash),
+            contentDescription = "Splash Screen",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillWidth
+        )
+    }
 }
