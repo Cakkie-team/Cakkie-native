10f6cfadca63397ed05973d7d625bd7af9f652ae
vcky4 <okon.victor.u@gmail.com>
2023-12-07 23:26:00 +0100

    create orders

---
+++ app/src/main/java/com/cakkie/ui/screens/orders/Orders.kt
@@ -0,0 +1,11 @@
+package com.cakkie.ui.screens.orders
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun Orders() {
+    Text(text = "Orders")
+}
