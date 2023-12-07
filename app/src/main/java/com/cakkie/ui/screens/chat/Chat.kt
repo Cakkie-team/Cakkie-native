062c1d6c627e665c10a4ef3a0041629f30de68c6
vcky4 <okon.victor.u@gmail.com>
2023-12-07 23:24:35 +0100

    create chat

---
+++ app/src/main/java/com/cakkie/ui/screens/chat/Chat.kt
@@ -0,0 +1,11 @@
+package com.cakkie.ui.screens.chat
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun Chat() {
+    Text(text = "Chat")
+}
---
+++ app/src/main/java/com/cakkie/ui/screens/chat/ChatViewModel.kt
@@ -0,0 +1,3 @@
+package com.cakkie.ui.screens.chat
+
+class ChatViewModel
