1d8afbcfe46f253a850121eedc3081480c5b98f4
vcky4 <okon.victor.u@gmail.com>
2023-12-07 23:17:59 +0100

    create shop

---
+++ app/src/main/java/com/cakkie/ui/screens/shop/Shop.kt
@@ -0,0 +1,11 @@
+package com.cakkie.ui.screens.shop
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun Shop() {
+    Text("Shop")
+}
---
+++ app/src/main/java/com/cakkie/ui/screens/shop/ShopViewModel.kt
@@ -0,0 +1,3 @@
+package com.cakkie.ui.screens.shop
+
+class ShopViewModel
