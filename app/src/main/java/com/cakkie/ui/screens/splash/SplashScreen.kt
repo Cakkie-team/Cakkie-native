ff8f34a11a1e56171cd598f48be3644f30624762
vcky4 <okon.victor.u@gmail.com>
2023-12-08 21:45:12 +0100

    complete log in

--- app/src/main/java/com/cakkie/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/cakkie/ui/screens/splash/SplashScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import com.cakkie.R
 import com.cakkie.ui.screens.destinations.EmailScreenDestination
+import com.cakkie.ui.screens.destinations.ExploreScreenDestination
 import com.cakkie.ui.screens.destinations.SplashScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
@@ -22,12 +24,21 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun SplashScreen(navigator: DestinationsNavigator) {
     val viewModel: SplashViewModel = koinViewModel()
+    val isLoggedIn = viewModel.isLoggedIn.collectAsState().value
     //navigate to the next screen after 2 seconds
     LaunchedEffect(key1 = Unit) {
         delay(2000)
-        navigator.navigate(EmailScreenDestination) {
-            popUpTo(SplashScreenDestination.route) {
-                inclusive = true
+        if (isLoggedIn) {
+            navigator.navigate(ExploreScreenDestination) {
+                popUpTo(SplashScreenDestination.route) {
+                    inclusive = true
+                }
+            }
+        } else {
+            navigator.navigate(EmailScreenDestination) {
+                popUpTo(SplashScreenDestination.route) {
+                    inclusive = true
+                }
             }
         }
     }
--- app/src/main/java/com/cakkie/ui/screens/splash/SplashViewModel.kt
+++ app/src/main/java/com/cakkie/ui/screens/splash/SplashViewModel.kt
@@ -1,19 +1,27 @@
 package com.cakkie.ui.screens.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.cakkie.datastore.Settings
+import com.cakkie.datastore.SettingsConstants
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 
 class SplashViewModel(private val settings: Settings) : ViewModel(), KoinComponent {
     private val _isLoggedIn = MutableStateFlow(false)
-    val isLoggedIn = _isLoggedIn
+    val isLoggedIn = _isLoggedIn.asStateFlow()
+
 
     init {
-//        viewModelScope.launch(Dispatchers.IO) {
-//            settings.getPreference(SettingsConstants.TOKEN, "").asLiveData().observeForever {
-//                _isLoggedIn.value = !it.isNullOrEmpty()
-//            }
-//        }
+        viewModelScope.launch {
+            settings.getPreference(SettingsConstants.TOKEN, "").asLiveData()
+                .observeForever {
+                    _isLoggedIn.value = it.isNotEmpty()
+                }
+        }
     }
+
 }
