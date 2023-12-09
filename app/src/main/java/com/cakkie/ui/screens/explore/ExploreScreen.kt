4bf23f52d41a34da78186fc145e4d4f930c8711b
vcky4 <okon.victor.u@gmail.com>
2023-12-09 12:23:48 +0100

    top bar done

--- app/src/main/java/com/cakkie/ui/screens/explore/ExploreScreen.kt
+++ app/src/main/java/com/cakkie/ui/screens/explore/ExploreScreen.kt
@@ -1,11 +1,80 @@
 package com.cakkie.ui.screens.explore
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.cakkie.R
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
 @Composable
 fun ExploreScreen() {
-    Text(text = "Explore")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_pic),
+                    contentDescription = "profile pic",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(shape = CircleShape)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = "Welcome \uD83D\uDC4B",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                    Text(
+                        text = "Cakkie",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.notification),
+                        contentDescription = "notification"
+                    )
+                }
+
+                IconButton(onClick = { /*TODO*/ }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.wallet),
+                        contentDescription = "wallet"
+                    )
+                }
+            }
+        }
+    }
 }
