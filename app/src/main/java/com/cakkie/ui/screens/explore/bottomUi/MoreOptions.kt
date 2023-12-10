b8b44d5478bcfb1e1c99d9e1ac79872ac2d303cc
vcky4 <okon.victor.u@gmail.com>
2023-12-10 17:38:41 +0100

    complete more screen

--- app/src/main/java/com/cakkie/ui/screens/explore/bottomUi/MoreOptions.kt
+++ app/src/main/java/com/cakkie/ui/screens/explore/bottomUi/MoreOptions.kt
@@ -1,12 +1,103 @@
 package com.cakkie.ui.screens.explore.bottomUi
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cakkie.R
+import com.cakkie.ui.theme.CakkieBrown
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.spec.DestinationStyleBottomSheet
 
 @Destination(style = DestinationStyleBottomSheet::class)
 @Composable
 fun MoreOptions() {
-    Text(text = "More Options")
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider(
+            Modifier
+                .clip(RoundedCornerShape(50))
+//                .height(8.dp)
+                .width(72.dp)
+                .align(Alignment.CenterHorizontally),
+            color = CakkieBrown,
+            thickness = 8.dp,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.share),
+                contentDescription = "share",
+                modifier = Modifier
+                    .size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = stringResource(id = R.string.share),
+                style = MaterialTheme.typography.labelLarge,
+                color = CakkieBrown,
+                fontSize = 16.sp,
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.flag),
+                contentDescription = "flag",
+                modifier = Modifier
+                    .size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = stringResource(id = R.string.flag),
+                style = MaterialTheme.typography.labelLarge,
+                color = CakkieBrown,
+                fontSize = 16.sp,
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.report),
+                contentDescription = "report",
+                modifier = Modifier
+                    .size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = stringResource(id = R.string.report),
+                style = MaterialTheme.typography.labelLarge,
+                color = CakkieBrown,
+                fontSize = 16.sp,
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
 }
