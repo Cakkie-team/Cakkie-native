5e763c937662541ffad6402ed08c5e752d939632
vcky4 <okon.victor.u@gmail.com>
2023-12-19 15:35:12 +0100

    fixed

--- app/src/main/java/com/cakkie/ui/screens/notification/AuthNotification.kt
+++ app/src/main/java/com/cakkie/ui/screens/notification/AuthNotification.kt
@@ -2,24 +2,30 @@ package com.cakkie.ui.screens.notification
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cakkie.R
 import com.cakkie.ui.components.CakkieButton
 import com.cakkie.ui.theme.CakkieBrown
+import com.cakkie.ui.theme.TextColorDark
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -30,13 +36,14 @@ fun AuthNotification(navigator: DestinationsNavigator) {
     Column(
         Modifier
             .padding(vertical = 30.dp, horizontal = 16.dp)
-            .fillMaxSize()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.arrow_back),
             contentDescription = "Arrow Back",
-
             modifier = Modifier
+                .align(Alignment.Start)
                 .clickable {
                     navigator.popBackStack()
                 }
@@ -45,68 +52,82 @@ fun AuthNotification(navigator: DestinationsNavigator) {
         Spacer(modifier = Modifier.height(80.dp))
         Text(
             text = stringResource(id = R.string.was_this_you),
-            style = MaterialTheme.typography.headlineMedium,
+            style = MaterialTheme.typography.bodyLarge,
             color = CakkieBrown,
-
-            )
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 25.sp
+        )
         Spacer(modifier = Modifier.height(5.dp))
-
 
         Text(
             text = stringResource(id = R.string.unknown_device),
-            style = MaterialTheme.typography.bodyMedium
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth(0.7f),
+            textAlign = TextAlign.Center,
+            color = TextColorDark.copy(alpha = 0.7f)
         )
         Spacer(modifier = Modifier.height(30.dp))
 
-        Row(modifier = Modifier.padding(horizontal = 35.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
 
-            Column() {
+            Column {
                 Text(
                     text = stringResource(id = R.string.location),
-                    style = MaterialTheme.typography.titleMedium,
+                    style = MaterialTheme.typography.bodyLarge,
                     color = CakkieBrown
 
                 )
                 Text(
                     text = stringResource(id = R.string.ikosi_ketu),
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = TextColorDark.copy(alpha = 0.7f)
                 )
                 Text(
                     text = stringResource(id = R.string.lagos_nigeria),
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = TextColorDark.copy(alpha = 0.7f)
                 )
             }
-            Spacer(modifier = Modifier.width(30.dp))
 
-            Column() {
+            Column {
                 Text(
                     text = stringResource(id = R.string.duration),
-                    style = MaterialTheme.typography.titleMedium,
+                    style = MaterialTheme.typography.bodyLarge,
                     color = CakkieBrown
 
                 )
                 Text(
                     text = stringResource(id = R.string.greenish_meridian),
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = TextColorDark.copy(alpha = 0.7f)
                 )
                 Text(
                     text = stringResource(id = R.string.date),
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = TextColorDark.copy(alpha = 0.7f)
                 )
             }
 
         }
         Spacer(modifier = Modifier.height(30.dp))
-        Text(
-            text = stringResource(id = R.string.device),
-            style = MaterialTheme.typography.titleMedium,
-            color = CakkieBrown
+        Column {
+            Text(
+                text = stringResource(id = R.string.device),
+                style = MaterialTheme.typography.bodyLarge,
+                color = CakkieBrown
 
-        )
-        Text(
-            text = stringResource(id = R.string.model),
-            style = MaterialTheme.typography.bodyMedium
-        )
+            )
+            Text(
+                text = stringResource(id = R.string.model),
+                style = MaterialTheme.typography.bodyLarge,
+                color = TextColorDark.copy(alpha = 0.7f)
+            )
+        }
         Spacer(modifier = Modifier.height(52.dp))
 
         CakkieButton(
@@ -117,7 +138,7 @@ fun AuthNotification(navigator: DestinationsNavigator) {
 
         Text(
             text = stringResource(id = R.string.no_it_was_not_me),
-            style = MaterialTheme.typography.labelMedium
+            style = MaterialTheme.typography.bodyLarge
         )
     }
 }
