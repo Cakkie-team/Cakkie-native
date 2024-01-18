ac350ace601af6545b71d82417f05b1062f04377
vcky4 <okon.victor.u@gmail.com>
2024-01-18 10:17:18 +0100

    replace splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,5 +139,7 @@ dependencies {
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.media3.exoplayer.hls)
 
-
+    //coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.gif)
 }
--- app/src/main/java/com/cakkie/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/cakkie/ui/screens/splash/SplashScreen.kt
@@ -1,14 +1,24 @@
 package com.cakkie.ui.screens.splash
 
+import android.os.Build.VERSION.SDK_INT
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import coil.size.Size
 import com.cakkie.R
 import com.cakkie.ui.screens.destinations.EmailScreenDestination
 import com.cakkie.ui.screens.destinations.ExploreScreenDestination
@@ -25,6 +35,16 @@ fun SplashScreen(navigator: DestinationsNavigator) {
     val viewModel: SplashViewModel = koinViewModel()
     val isLoggedIn = viewModel.isLoggedIn.collectAsState().value
     val isReady = viewModel.isReady.collectAsState().value
+    val context = LocalContext.current
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
     //navigate to the next screen after 2 seconds
     LaunchedEffect(key1 = isReady) {
         if (isReady) {
@@ -48,6 +68,18 @@ fun SplashScreen(navigator: DestinationsNavigator) {
             painter = painterResource(id = R.drawable.splash),
             contentDescription = "Splash Screen",
             modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillWidth
+        )
+        Image(
+            painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(context).data(data = R.drawable.splash_ann).apply(block = {
+                    size(Size.ORIGINAL)
+                }).build(), imageLoader = imageLoader
+            ),
+            contentDescription = "Splash animation",
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .align(Alignment.Center),
             contentScale = ContentScale.FillWidth
         )
     }
