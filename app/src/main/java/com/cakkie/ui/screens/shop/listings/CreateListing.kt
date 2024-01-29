a08031e9f1b74247b7c0aa0a6ed6cd211453fb50
vcky4 <okon.victor.u@gmail.com>
2024-01-29 20:35:05 +0100

    feet: filter images

--- app/src/main/java/com/cakkie/ui/screens/shop/listings/CreateListing.kt
+++ app/src/main/java/com/cakkie/ui/screens/shop/listings/CreateListing.kt
@@ -3,9 +3,11 @@ package com.cakkie.ui.screens.shop.listings
 import android.Manifest.permission.READ_MEDIA_IMAGES
 import android.Manifest.permission.READ_MEDIA_VIDEO
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,17 +17,24 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.IconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -58,17 +67,18 @@ fun CreateListing(navigator: DestinationsNavigator) {
     val medias = remember {
         mutableStateListOf<MediaModel>()
     }
+    var filter by remember {
+        mutableIntStateOf(R.string.all)
+    }
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
     val files = remember {
         mutableStateListOf<String>()
     }
-    val permissionState =
-        rememberMultiplePermissionsState(
-            permissions = listOf(
-                READ_MEDIA_IMAGES,
-                READ_MEDIA_VIDEO
-            )
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            READ_MEDIA_IMAGES, READ_MEDIA_VIDEO
         )
+    )
 
     LaunchedEffect(key1 = permissionState) {
         if (permissionState.allPermissionsGranted) {
@@ -88,19 +98,42 @@ fun CreateListing(navigator: DestinationsNavigator) {
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.arrow_back),
-                    contentDescription = "Back", contentScale = ContentScale.FillWidth,
+                    contentDescription = "Back",
+                    contentScale = ContentScale.FillWidth,
                     modifier = Modifier.width(24.dp)
                 )
             }
-            Text(
-                stringResource(id = R.string.shop),
-                style = MaterialTheme.typography.bodyLarge,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.SemiBold,
-                color = CakkieBrown,
-                modifier = Modifier
+            Row(
+                Modifier
                     .align(Alignment.Center)
-            )
+                    .clip(RoundedCornerShape(bottomStart = 15.dp, topEnd = 15.dp))
+                    .background(
+                        CakkieBrown.copy(alpha = 0.5f)
+                    ),
+            ) {
+                listOf(
+                    R.string.all, R.string.images, R.string.videos
+                ).forEach {
+                    Box(
+                        Modifier
+                            .clickable { filter = it }
+                            .background(
+                                if (it == filter) {
+                                    CakkieBrown
+                                } else {
+                                    CakkieBrown.copy(alpha = 0.5f)
+                                }
+                            )
+                            .height(35.dp),
+                        contentAlignment = Alignment.Center) {
+                        Text(
+                            text = stringResource(id = it),
+                            modifier = Modifier.padding(8.dp),
+                            color = CakkieBackground
+                        )
+                    }
+                }
+            }
 
             Text(
                 stringResource(id = R.string.selected, files.size),
@@ -119,14 +152,25 @@ fun CreateListing(navigator: DestinationsNavigator) {
 
             if (permissionState.allPermissionsGranted) {
                 LazyVerticalGrid(
+                    modifier = Modifier.padding(horizontal = 2.dp),
                     columns = GridCells.Fixed(3)
                 ) {
                     items(
-                        items = medias
+                        items = medias.filter {
+                            when (filter) {
+                                R.string.all -> true
+                                R.string.images -> !it.isVideo
+                                R.string.videos -> it.isVideo
+                                else -> true
+                            }
+                        },
                     ) { item ->
-                        Box(modifier = Modifier.fillMaxWidth()) {
-                            GlideImage(
-                                model = item.uri,
+                        Box(
+                            modifier = Modifier
+                                .padding(2.dp)
+                                .fillMaxWidth()
+                        ) {
+                            GlideImage(model = item.uri,
                                 contentDescription = "cake",
                                 modifier = Modifier
                                     .clickable {
@@ -143,24 +187,33 @@ fun CreateListing(navigator: DestinationsNavigator) {
                                     }
                                     .fillMaxWidth()
                                     .height(screenWidth / 3),
-                                contentScale = ContentScale.Crop
-                            )
-
+                                contentScale = ContentScale.Crop)
+                            if (item.isVideo) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.video),
+                                    contentDescription = stringResource(
+                                        id = R.string.video
+                                    ),
+                                    tint = CakkieBackground,
+                                    modifier = Modifier
+                                        .size(30.dp)
+                                        .padding(4.dp)
+                                        .background(Color.Black.copy(alpha = 0.5f))
+                                        .align(Alignment.TopStart)
+                                )
+                            }
                             Checkbox(
-                                checked = files.contains(item.toString()),
-                                onCheckedChange = {
+                                checked = files.contains(item.toString()), onCheckedChange = {
                                     if (!files.contains(item.toString()) && files.size < 5) {
                                         files.add(item.toString())
                                     } else {
                                         files.remove(item.toString())
                                     }
-                                },
-                                modifier = Modifier
+                                }, modifier = Modifier
                                     .size(20.dp)
                                     .align(
                                         Alignment.TopEnd
-                                    ),
-                                colors = CheckboxDefaults.colors(
+                                    ), colors = CheckboxDefaults.colors(
                                     checkmarkColor = CakkieBackground,
                                     checkedColor = CakkieBrown,
                                     uncheckedColor = CakkieBrown.copy(alpha = 0.5f)
@@ -171,8 +224,7 @@ fun CreateListing(navigator: DestinationsNavigator) {
                 }
             } else {
                 Column(
-                    Modifier
-                        .align(Alignment.Center),
+                    Modifier.align(Alignment.Center),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
@@ -183,13 +235,14 @@ fun CreateListing(navigator: DestinationsNavigator) {
                     )
                     CakkieButton(
                         text = stringResource(id = R.string.grant_permission),
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
                     ) {
                         permissionState.launchMultiplePermissionRequest()
                     }
                 }
             }
+
+
             CakkieButton(
                 text = stringResource(id = R.string.done),
                 enabled = files.size > 0,
