1d84db441cee2797e94fe220550ad54a52f18a0e
vcky4 <okon.victor.u@gmail.com>
2024-01-29 19:41:06 +0100

    get device images

--- app/src/main/java/com/cakkie/ui/screens/shop/ShopViewModel.kt
+++ app/src/main/java/com/cakkie/ui/screens/shop/ShopViewModel.kt
@@ -1,10 +1,14 @@
 package com.cakkie.ui.screens.shop
 
+import android.content.Context
 import android.location.Address
+import android.net.Uri
+import android.provider.MediaStore
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.loader.content.CursorLoader
 import com.cakkie.data.db.models.User
 import com.cakkie.data.repositories.UserRepository
 import com.cakkie.networkModels.LoginResponse
@@ -13,6 +17,7 @@ import com.cakkie.utill.NetworkCalls
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import timber.log.Timber
 import java.io.File
 
 class ShopViewModel : ViewModel(), KoinComponent {
@@ -53,13 +58,88 @@ class ShopViewModel : ViewModel(), KoinComponent {
             )
         )
 
-
     fun uploadImage(image: File, path: String, fileName: String) =
         NetworkCalls.uploadFile(
             endpoint = Endpoints.UPLOAD_IMAGE(path, fileName), media = image
         )
 
+    fun getMedias(context: Context): List<MediaModel> {
+        val files = mutableListOf<MediaModel>()
+        val projections = arrayOf(
+            MediaStore.Files.FileColumns._ID,
+            MediaStore.Files.FileColumns.DATA,
+            MediaStore.Files.FileColumns.DATE_ADDED,
+            MediaStore.Files.FileColumns.MEDIA_TYPE,
+            MediaStore.Files.FileColumns.MIME_TYPE,
+            MediaStore.Files.FileColumns.TITLE
+        )
+
+// This will Return only video and image metadata.
+        val selection = (MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO)
+
+        val queryUri = MediaStore.Files.getContentUri("external")
+        val cursorLoader = CursorLoader(
+            context,
+            queryUri,
+            projections,
+            selection,
+            null,  // Selection args (none).
+            MediaStore.Files.FileColumns.DATE_ADDED + " DESC" // Sort order.
+        ).loadInBackground()
+//        Timber.d("cursor: ${cursorLoader?.count}")
+        viewModelScope.launch {
+            if (cursorLoader != null) {
+                //clear all previous data
+//                files.clearAll()
+
+                val media = cursorLoader.getColumnIndex(MediaStore.Files.FileColumns._ID)
+                val mediaDate = cursorLoader.getColumnIndex(MediaStore.Files.FileColumns.DATE_ADDED)
+                val mediaType = cursorLoader.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE)
+                while (cursorLoader.moveToNext()) {
+                    if (cursorLoader.getInt(mediaType) == MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE) {
+                        files.add(
+                            MediaModel(
+                                uri = Uri.withAppendedPath(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    cursorLoader.getString(media)
+                                ), dateAdded = cursorLoader.getLong(mediaDate),
+                                isVideo = false
+                            )
+
+                        )
+                    } else if (cursorLoader.getInt(mediaType) == MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO) {
+                        files.add(
+                            MediaModel(
+                                uri = Uri.withAppendedPath(
+                                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                                    cursorLoader.getString(media)
+                                ), dateAdded = cursorLoader.getLong(mediaDate),
+                                isVideo = true
+                            )
+                        )
+                    }
+                }
+//                repo.insertMediaList(gelImedia)
+//                Timber.d("gelImedia: $files")
+            } else {
+                Timber.d("cursorLoader is null: $cursorLoader")
+            }
+
+        }
+        return files
+    }
+
     init {
         getUser()
     }
 }
+
+data class MediaModel(
+    val uri: Uri,
+    val dateAdded: Long,
+    val isVideo: Boolean
+)
--- app/src/main/java/com/cakkie/ui/screens/shop/listings/CreateListing.kt
+++ app/src/main/java/com/cakkie/ui/screens/shop/listings/CreateListing.kt
@@ -1,5 +1,7 @@
 package com.cakkie.ui.screens.shop.listings
 
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.Manifest.permission.READ_MEDIA_VIDEO
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -12,18 +14,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Checkbox
 import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -33,20 +38,47 @@ import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.cakkie.R
 import com.cakkie.ui.components.CakkieButton
+import com.cakkie.ui.screens.shop.MediaModel
+import com.cakkie.ui.screens.shop.ShopViewModel
 import com.cakkie.ui.theme.CakkieBackground
 import com.cakkie.ui.theme.CakkieBrown
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import org.koin.androidx.compose.koinViewModel
 import timber.log.Timber
 
-@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalPermissionsApi::class)
 @Destination
 @Composable
 fun CreateListing(navigator: DestinationsNavigator) {
+    val viewModel: ShopViewModel = koinViewModel()
+    val context = LocalContext.current
+    val medias = remember {
+        mutableStateListOf<MediaModel>()
+    }
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
     val files = remember {
         mutableStateListOf<String>()
     }
+    val permissionState =
+        rememberMultiplePermissionsState(
+            permissions = listOf(
+                READ_MEDIA_IMAGES,
+                READ_MEDIA_VIDEO
+            )
+        )
+
+    LaunchedEffect(key1 = permissionState) {
+        if (permissionState.allPermissionsGranted) {
+            medias.addAll(viewModel.getMedias(context))
+        } else {
+            permissionState.launchMultiplePermissionRequest()
+        }
+    }
+
+//    Timber.d(medias.toString())
     Column(Modifier.fillMaxSize()) {
 //        Spacer(modifier = Modifier.height(20.dp))
         Box(Modifier.fillMaxWidth()) {
@@ -81,52 +113,80 @@ fun CreateListing(navigator: DestinationsNavigator) {
                     .align(Alignment.CenterEnd)
             )
         }
-        Box {
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(3)
-            ) {
-                items(100) { item ->
-                    Box(modifier = Modifier.fillMaxWidth()) {
-                        GlideImage(
-                            model = "https://source.unsplash.com/400x400/?cakes?$item",
-                            contentDescription = "cake",
-                            modifier = Modifier
-                                .clickable {
+        Box(
+            Modifier.fillMaxSize()
+        ) {
+
+            if (permissionState.allPermissionsGranted) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(3)
+                ) {
+                    items(
+                        items = medias
+                    ) { item ->
+                        Box(modifier = Modifier.fillMaxWidth()) {
+                            GlideImage(
+                                model = item.uri,
+                                contentDescription = "cake",
+                                modifier = Modifier
+                                    .clickable {
+                                        if (!files.contains(item.toString()) && files.size < 5) {
+                                            files.add(item.toString())
+                                        } else {
+                                            files.remove(item.toString())
+                                        }
+                                        Timber.d(
+                                            files
+                                                .contains(item.toString())
+                                                .toString()
+                                        )
+                                    }
+                                    .fillMaxWidth()
+                                    .height(screenWidth / 3),
+                                contentScale = ContentScale.Crop
+                            )
+
+                            Checkbox(
+                                checked = files.contains(item.toString()),
+                                onCheckedChange = {
                                     if (!files.contains(item.toString()) && files.size < 5) {
                                         files.add(item.toString())
                                     } else {
                                         files.remove(item.toString())
                                     }
-                                    Timber.d(
-                                        files
-                                            .contains(item.toString())
-                                            .toString()
-                                    )
-                                }
-                                .fillMaxWidth()
-                                .height(screenWidth / 3),
-                            contentScale = ContentScale.FillBounds
-                        )
-                        Checkbox(
-                            checked = files.contains(item.toString()),
-                            onCheckedChange = {
-                                if (!files.contains(item.toString()) && files.size < 5) {
-                                    files.add(item.toString())
-                                } else {
-                                    files.remove(item.toString())
-                                }
-                            },
-                            modifier = Modifier
-                                .size(20.dp)
-                                .align(
-                                    Alignment.TopEnd
-                                ),
-                            colors = CheckboxDefaults.colors(
-                                checkmarkColor = CakkieBackground,
-                                checkedColor = CakkieBrown,
-                                uncheckedColor = CakkieBrown.copy(alpha = 0.5f)
+                                },
+                                modifier = Modifier
+                                    .size(20.dp)
+                                    .align(
+                                        Alignment.TopEnd
+                                    ),
+                                colors = CheckboxDefaults.colors(
+                                    checkmarkColor = CakkieBackground,
+                                    checkedColor = CakkieBrown,
+                                    uncheckedColor = CakkieBrown.copy(alpha = 0.5f)
+                                )
                             )
-                        )
+                        }
+                    }
+                }
+            } else {
+                Column(
+                    Modifier
+                        .align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.media_permission_required),
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontSize = 16.sp,
+                        color = CakkieBrown,
+                    )
+                    CakkieButton(
+                        text = stringResource(id = R.string.grant_permission),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        permissionState.launchMultiplePermissionRequest()
                     }
                 }
             }
