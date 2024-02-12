364435316f4b24ebb140964284dd1147525f449b
vcky4 <okon.victor.u@gmail.com>
2024-02-12 19:06:41 +0100

    fix reviews

--- app/src/main/java/com/cakkie/ui/screens/explore/Reviews.kt
+++ app/src/main/java/com/cakkie/ui/screens/explore/Reviews.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,23 +16,62 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.cakkie.R
+import com.cakkie.ui.theme.CakkieBrown
 import com.cakkie.ui.theme.CakkieYellow
+import com.cakkie.ui.theme.TextColorInactive
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun Reviews() {
-    Column {
-        repeat(5) {
+    val reviews by remember {
+        mutableStateOf(listOf<String>())
+    }
+    Column(Modifier.fillMaxSize()) {
+        if (reviews.isEmpty()) {
+            Column(
+                Modifier
+                    .fillMaxWidth(0.8f)
+                    .padding(top = 30.dp)
+                    .align(Alignment.CenterHorizontally),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.oh_oh),
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = CakkieBrown,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    text = stringResource(id = R.string.sorry_no_reviews_yet),
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = TextColorInactive,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+        reviews.forEach { _ ->
             Row(
                 Modifier
                     .padding(horizontal = 16.dp)
