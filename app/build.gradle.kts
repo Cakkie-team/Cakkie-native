5cd37cf97709a5f54d58c3ece4832669fab87333
vcky4 <okon.victor.u@gmail.com>
2024-03-21 00:02:55 +0100

    uograde

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.cakkie"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.02.1"
+        versionCode = 6
+        versionName = "1.02.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
