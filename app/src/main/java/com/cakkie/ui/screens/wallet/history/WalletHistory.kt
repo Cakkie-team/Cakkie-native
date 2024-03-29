bd2a04a0fad57a170eb9f2c9f66d60964748180e
vcky4 <okon.victor.u@gmail.com>
2024-03-29 10:55:49 +0100

    fix: back button functionality and size

--- app/src/main/java/com/cakkie/ui/screens/wallet/history/WalletHistory.kt
+++ app/src/main/java/com/cakkie/ui/screens/wallet/history/WalletHistory.kt
@@ -1,7 +1,6 @@
 package com.cakkie.ui.screens.wallet.history
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,8 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,25 +29,30 @@ import androidx.compose.ui.unit.sp
 import com.cakkie.R
 import com.cakkie.ui.screens.wallet.components.WalletFilter
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination
 @Composable
-fun WalletHistory() {
+fun WalletHistory(navigator: DestinationsNavigator) {
     var filter by remember { mutableStateOf("All") }
     val history = listOf<String>()
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 20.dp),
+            .padding(top = 10.dp),
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp),
+                .padding(start = 6.dp),
             contentAlignment = Alignment.CenterStart
         ) {
-            Image(painter = painterResource(id = R.drawable.arrow_back),
-                contentDescription = "", modifier = Modifier.clickable { })
+            IconButton(onClick = { navigator.popBackStack() }) {
+                Image(
+                    painter = painterResource(id = R.drawable.arrow_back),
+                    contentDescription = "go back", modifier = Modifier.size(24.dp)
+                )
+            }
             Text(
                 text = stringResource(id = R.string.transaction_history),
                 style = MaterialTheme.typography.bodyLarge,
@@ -54,7 +60,7 @@ fun WalletHistory() {
                 fontSize = 18.sp
             )
         }
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(10.dp))
         WalletFilter(filter) {
             filter = it
         }
@@ -69,9 +75,11 @@ fun WalletHistory() {
                 }
             }
         } else {
-            Box(modifier = Modifier
-                .fillMaxHeight(0.6f)
-                .fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.6f)
+                    .fillMaxWidth()
+            ) {
                 Text(
                     text = "No history found",
                     style = MaterialTheme.typography.bodyMedium,
