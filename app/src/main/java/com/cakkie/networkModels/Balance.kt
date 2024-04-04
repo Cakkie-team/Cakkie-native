c4481c2d592b3fdbeef3183ccab805b3fe827e8e
vcky4 <okon.victor.u@gmail.com>
2024-04-04 01:26:23 +0100

    feet: add balance model

---
+++ app/src/main/java/com/cakkie/networkModels/Balance.kt
@@ -0,0 +1,22 @@
+package com.cakkie.networkModels
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Parcelize
+data class Balance(
+    val balance: Double = 0.0,
+    val createdAt: String,
+    val credit: Double = 0.0,
+    val debit: Double = 0.0,
+    val exchangeRate: Double = 0.0,
+    val id: String,
+    val isActive: Boolean,
+    val name: String,
+    val symbol: String,
+    val type: String,
+    val updatedAt: String,
+    val withdrawalFee: Double = 0.0,
+) : Parcelable
