aa8807e31e66568e5c6fc01c470a0c42b3f69c09
vcky4 <okon.victor.u@gmail.com>
2024-04-16 08:53:20 +0100

    incrase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.cakkie"
         minSdk = 24
         targetSdk = 34
-        versionCode = 24
-        versionName = "1.04.1"
+        versionCode = 25
+        versionName = "1.04.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
