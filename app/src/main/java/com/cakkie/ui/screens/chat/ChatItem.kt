ea9ec92dd11d38fb56cf12e31acbe7c3190e9204
vcky4 <okon.victor.u@gmail.com>
2024-04-23 01:01:36 +0100

    swipe progress

--- app/src/main/java/com/cakkie/ui/screens/chat/ChatItem.kt
+++ app/src/main/java/com/cakkie/ui/screens/chat/ChatItem.kt
@@ -1,13 +1,19 @@
 package com.cakkie.ui.screens.chat
 
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -16,22 +22,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cakkie.ui.theme.CakkieBackground
 import com.cakkie.ui.theme.CakkieBrown
 import com.cakkie.ui.theme.TextColorDark
+import kotlin.math.roundToInt
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ChatItem(item: Int) {
     val config = LocalConfiguration.current
     val width = config.screenWidthDp.dp
+    val swipeableState = rememberSwipeableState(0)
+    // Reset swipeable state after swipe ends
+//    LaunchedEffect(swipeableState) {
+//        snapshotFlow { swipeableState.offset }
+//            .filter {
+//                it.value != 0f && swipeableState.currentValue.toFloat() == 1f
+//            }
+//            .collect {
+//                swipeableState.animateTo(0)
+//            }
+//    }
     Row(
-        Modifier.fillMaxWidth(),
+        Modifier
+            .swipeable(
+                state = swipeableState,
+                anchors = mapOf(0f to 0, width.value to 1),
+                thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                orientation = Orientation.Horizontal,
+            )
+            .fillMaxWidth(),
         horizontalArrangement = if (item % 2 == 0) Arrangement.Start else Arrangement.End
     ) {
         Card(
             Modifier
+                .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
                 .padding(horizontal = 16.dp, vertical = 8.dp),
             shape = CardDefaults.elevatedShape,
             colors = CardDefaults.cardColors(
